ea0b406f5bb0a91338d668eec49e2d752879ff64
ayumi0511 <ayumi.tokoeda@gmail.com>
2023-11-23 20:18:21 +0900

    詳細ページ表示

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -27,6 +27,10 @@ class TweetsController < ApplicationController
     tweet.update(tweet_params)
     redirect_to root_path
   end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
   
   private
   def tweet_params
