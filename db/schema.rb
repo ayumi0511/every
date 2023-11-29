391d762959a3d07721df842f6778e71943016859
ayumi0511 <ayumi.tokoeda@gmail.com>
2023-11-29 14:48:36 +0900

    tweetsテーブルにuser_idカラムをinteger型で追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_27_043519) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_29_053934) do
   create_table "tweets", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.string "text"
     t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
