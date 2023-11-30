adb510fd0800cd4a47849ff312e104fdc85950ab
ayumi0511 <ayumi.tokoeda@gmail.com>
2023-11-30 20:54:39 +0900

    投稿者のマイページにアクセス

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,8 +17,9 @@ class UsersController < ApplicationController
     end
   
     def show
-      @nickname = current_user.nickname
-      @tweets = current_user.tweets
+      user = User.find(params[:id])
+      @nickname = user.nickname
+      @tweets = user.tweets
     end
   
     def new
